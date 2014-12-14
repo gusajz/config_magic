5dbda19f18cdd69fbe18b4a0e0c6fc9e1129079f
Gustavo Ajzenman <gustavoajz@gmail.com>
2014-12-14 20:59:45 -0300

    find_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="config_magic",
     version="0.0.1",
+
+    packages=find_packages('./'),
     
     install_requires=[
     ],

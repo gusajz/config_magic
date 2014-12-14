990539af565cbecb5ac30c1910c50458a0e8f38f
Gustavo Ajzenman <gustavoajz@gmail.com>
2014-12-14 20:47:32 -0300

    Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="config_magic",
+    version="0.0.1",
+    
+    install_requires=[
+    ],
+    
+    author='Gustavo Ajzenman',
+    author_email=''
+)
